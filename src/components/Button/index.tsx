e7930bb976c632bb6967e6ef391a64d3dccb849e
Daegyeom Ha <combbm@gmail.com>
2024-12-15 20:22:07 +0900

    feat: implement `as` prop in `Button` component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,30 +1,35 @@
-import { ButtonHTMLAttributes, FC, PropsWithChildren } from 'react';
+import { ButtonHTMLAttributes, ComponentPropsWithoutRef, ComponentType, ElementType, PropsWithChildren } from 'react';
 import { cn } from '@/utils/cn';
 
-interface ButtonProps {
+interface BaseButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   readonly variance?: 'primary' | 'text';
   readonly active?: boolean;
 }
 
-const Button: FC<PropsWithChildren<ButtonHTMLAttributes<unknown> & ButtonProps>> = ({
+type ButtonProps<E extends ElementType | ComponentType = 'button'> = BaseButtonProps & {
+  readonly as?: E;
+} & Omit<ComponentPropsWithoutRef<E>, keyof BaseButtonProps>;
+
+const Button = <C extends ElementType = 'button'>({
   variance = 'primary',
   active,
   children,
   className,
+  as,
   ...props
-}) => {
+}: PropsWithChildren<ButtonProps<C>>) => {
+  const Tag = as || 'button';
   const styleMap = {
     primary: '',
     text: 'bg-transparent text-black hover:bg-gray-200'
   };
-
   const activeStyleMap = {
     primary: '',
     text: 'bg-gray-200'
   };
 
   return (
-    <button
+    <Tag
       type="button"
       className={cn(
         'cursor-pointer rounded-md px-3 py-2 text-black',
@@ -34,7 +39,7 @@ const Button: FC<PropsWithChildren<ButtonHTMLAttributes<unknown> & ButtonProps>>
       {...props}
     >
       {children}
-    </button>
+    </Tag>
   );
 };
 
