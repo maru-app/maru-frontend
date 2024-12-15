a48207def85729a29c597e3eac3c4ca804b3fd7d
Daegyeom Ha <combbm@gmail.com>
2024-12-15 17:10:53 +0900

    feat: change active style of `Navbar` according to pathname

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,30 @@
+'use client';
+
 import { FC } from 'react';
 import Container from '@/components/Container';
 import Button from '@/components/Button';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar: FC = () => {
+  const pathname = usePathname();
+
   return (
     <header className="fixed w-full border-b bg-white/50 py-3 backdrop-blur">
       <Container className="flex items-center justify-between">
-        <h1 className="font-hahmlet text-[26px] font-bold">마루</h1>
+        <Link className="select-none font-hahmlet text-[26px] font-bold" href="/">
+          마루
+        </Link>
         <div className="flex space-x-8">
-          <Button variance="text" active>
+          <Button variance="text" active={pathname === '/'}>
             소개
           </Button>
-          <Button variance="text">랭킹</Button>
-          <Button variance="text">개인정보</Button>
+          <Button variance="text" active={pathname === '/rank'}>
+            랭킹
+          </Button>
+          <Button variance="text" active={pathname === '/privacy'}>
+            개인정보
+          </Button>
         </div>
         <Button className="bg-emerald-500 text-sm font-bold text-white hover:bg-emerald-600">로그인</Button>
       </Container>
