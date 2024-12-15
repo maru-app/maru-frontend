2ded790f95dc31c304716dc8a3ba8f529d4931e6
Daegyeom Ha <combbm@gmail.com>
2024-12-15 20:00:13 +0900

    feat: implement some menu when user has permission

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,27 +6,46 @@ import Button from '@/components/Button';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-const Navbar: FC = () => {
+interface NavbarProps {
+  readonly authorize?: boolean;
+}
+
+const Navbar: FC<NavbarProps> = ({ authorize }) => {
   const pathname = usePathname();
 
   return (
     <header className="fixed w-full border-b bg-white/50 py-3 backdrop-blur">
       <Container className="flex items-center justify-between">
-        <Link className="select-none font-hahmlet text-[26px] font-bold" href="/">
-          마루
-        </Link>
-        <div className="flex space-x-8">
-          <Button variance="text" active={pathname === '/'}>
-            소개
-          </Button>
-          <Button variance="text" active={pathname === '/rank'}>
-            랭킹
-          </Button>
-          <Button variance="text" active={pathname === '/privacy'}>
-            개인정보
-          </Button>
+        <div className="flex">
+          <Link className="mr-16 select-none font-hahmlet text-[26px] font-bold" href="/">
+            마루
+          </Link>
+          <div className="flex space-x-6">
+            <div className="flex space-x-4">
+              <Button variance="text" active={pathname === '/'}>
+                소개
+              </Button>
+              <Button variance="text" active={pathname === '/rank'}>
+                랭킹
+              </Button>
+              <Button variance="text" active={pathname === '/privacy'}>
+                개인정보
+              </Button>
+            </div>
+            {authorize && (
+              <>
+                <div className="my-1 border-r" />
+                <div className="flex space-x-4">
+                  <Button variance="text">내 일기</Button>
+                  <Button variance="text">프로필</Button>
+                </div>
+              </>
+            )}
+          </div>
         </div>
-        <Button className="bg-emerald-500 text-sm font-bold text-white hover:bg-emerald-600">로그인</Button>
+        <Button className="bg-emerald-500 text-sm font-bold text-white hover:bg-emerald-600">
+          {authorize ? '일기쓰기' : '로그인'}
+        </Button>
       </Container>
     </header>
   );
