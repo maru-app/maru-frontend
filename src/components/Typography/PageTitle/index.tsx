34207603653a34e5ccecfe2879908c2db4c23cf8
Daegyeom Ha <combbm@gmail.com>
2024-12-16 15:00:54 +0900

    feat: implement `PageTitle` component

---
+++ src/components/Typography/PageTitle/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+
+interface PageTitleProps {
+  readonly title: string;
+  readonly description: string;
+}
+
+const PageTitle: FC<PageTitleProps> = ({ title, description }) => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">{title}</h1>
+      <p className="text-lg font-bold">{description}</p>
+    </>
+  );
+};
+
+export default PageTitle;
