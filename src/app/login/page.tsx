d897a7ff8bcd00a5da82a75f970b646d1a4eace6
Daegyeom Ha <combbm@gmail.com>
2025-01-02 21:06:35 +0900

    feat: add hyperlink to Login page for social login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import Button from '@/components/Button';
+import { API_HOST } from '@/api';
 
 const Page: FC = () => {
   return (
@@ -8,9 +9,19 @@ const Page: FC = () => {
 
       <div className="flex flex-col space-y-2">
         {/* TODO: 실제 소셜로그인 디자인 가이드 라인을 사용할 것. */}
-        <Button className="min-w-60 bg-green-500 text-white">네이버 로그인</Button>
-        <Button className="min-w-60 bg-blue-500 text-white">Google 로그인</Button>
-        <Button className="min-w-60 bg-neutral-900 text-white">Apple 로그인</Button>
+        <Button className="min-w-60 bg-green-500 text-center text-white" as="a" href={`${API_HOST}/oauth/login/naver`}>
+          네이버 로그인
+        </Button>
+        <Button className="min-w-60 bg-blue-500 text-center text-white" as="a" href={`${API_HOST}/oauth/login/google`}>
+          Google 로그인
+        </Button>
+        <Button
+          className="min-w-60 bg-neutral-900 text-center text-white"
+          as="a"
+          href={`${API_HOST}/oauth/login/apple`}
+        >
+          Apple 로그인
+        </Button>
       </div>
     </div>
   );
