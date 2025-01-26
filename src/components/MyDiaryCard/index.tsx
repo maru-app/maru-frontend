ad6908fc540255d2332a0ea749da3e8f13f3b897
Daegyeom Ha <combbm@gmail.com>
2025-01-26 19:53:04 +0900

    fix: change border thickness and apply line clamp

--- src/components/MyDiaryCard/index.tsx
+++ src/components/MyDiaryCard/index.tsx
@@ -7,9 +7,9 @@ interface MyDiaryCardProps {
 
 const MyDiaryCard: FC<MyDiaryCardProps> = ({ title, date }) => {
   return (
-    <div className="group mt-2 cursor-pointer rounded-md border-2 border-gray-200 p-4 transition-all duration-200 ease-in-out hover:bg-gray-200">
-      <p className="break-keep text-lg font-bold group-hover:underline">{title}</p>
-      <p className="text-lg">
+    <div className="group mt-2 cursor-pointer rounded-md border border-gray-200 p-4 transition-all duration-200 ease-in-out hover:bg-gray-200">
+      <p className="line-clamp-2 break-keep text-lg font-bold group-hover:underline">{title}</p>
+      <p className="mt-1.5 text-lg">
         {date.getFullYear()}년 {date.getMonth() + 1}월 {date.getDate()}일 일기
       </p>
       <p />
