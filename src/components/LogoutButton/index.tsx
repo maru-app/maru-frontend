95fac263feee8c81b8e3444880bec9dde833a28d
Daegyeom Ha <combbm@gmail.com>
2025-01-30 20:57:27 +0900

    feat: redirect to main page when logout

--- src/components/LogoutButton/index.tsx
+++ src/components/LogoutButton/index.tsx
@@ -3,10 +3,18 @@
 import { FC } from 'react';
 import Button from '@/components/Button';
 import { logoutMutation } from '@/api/mutation/auth-mutation';
+import { useRouter } from 'next/navigation';
 
 const LogoutButton: FC = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await logoutMutation();
+    await router.push('/');
+  };
+
   return (
-    <Button type="button" className="bg-red-500 px-5 text-white hover:bg-red-600" onClick={() => logoutMutation()}>
+    <Button type="button" className="bg-red-500 px-5 text-white hover:bg-red-600" onClick={handleLogout}>
       로그아웃
     </Button>
   );
