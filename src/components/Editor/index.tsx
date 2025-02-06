778c1fe77a4736f8ab52d042be6ee3af1abb820e
Daegyeom Ha <combbm@gmail.com>
2025-02-06 10:09:14 +0900

    feat: add `value` and `onChange` props in Editor component

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -5,6 +5,11 @@ import 'react-quill-new/dist/quill.snow.css';
 import { FC } from 'react';
 import dynamic from 'next/dynamic';
 
+interface EditorProps {
+  readonly value: string;
+  readonly onChange: (value: string) => void;
+}
+
 const QuillWrapper = dynamic(() => import('react-quill-new'), {
   ssr: false,
   loading: () => <div className="h-96 w-full border border-gray-200" />
@@ -15,7 +20,7 @@ const modules = {
     [{ header: '1' }, { header: '2' }, { header: '3' }],
     ['bold', 'italic', 'underline', 'strike', 'blockquote'],
     [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ color: [] }, { background: [] }],
     [{ align: [] }],
     ['link', 'image'],
     ['clean']
@@ -25,8 +30,8 @@ const modules = {
   }
 };
 
-const Editor: FC = () => {
-  return <QuillWrapper theme="snow" modules={modules} />;
+const Editor: FC<EditorProps> = ({ value, onChange }) => {
+  return <QuillWrapper theme="snow" modules={modules} value={value} onChange={onChange} />;
 };
 
 export default Editor;
