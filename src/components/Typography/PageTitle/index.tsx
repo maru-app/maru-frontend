795b03994189c97b1ae7bc0daaea2865a6f947c1
Daegyeom Ha <combbm@gmail.com>
2025-02-08 20:54:34 +0900

    feat: add div wrapper in PageTitle component

--- src/components/Typography/PageTitle/index.tsx
+++ src/components/Typography/PageTitle/index.tsx
@@ -7,10 +7,10 @@ interface PageTitleProps {
 
 const PageTitle: FC<PageTitleProps> = ({ title, description }) => {
   return (
-    <>
+    <div>
       <h1 className="text-3xl font-bold">{title}</h1>
       <p className="text-lg font-bold">{description}</p>
-    </>
+    </div>
   );
 };
 
