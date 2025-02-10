542df13414d683c325de8812ab09aac7fa930747
Daegyeom Ha <combbm@gmail.com>
2025-02-10 15:08:26 +0900

    feat: add mobile style in Strike component

--- src/components/Strike/index.tsx
+++ src/components/Strike/index.tsx
@@ -8,9 +8,11 @@ const Strike: FC = () => {
       <h3 className="mb-3 text-xl font-bold">
         지금까지 연속 기록을 <span className="text-emerald-500">1일</span> 유지 중이에요.
       </h3>
-      <div className="flex items-center justify-between">
-        <StrikeGraph />
-        <div className="space-y-2">
+      <div className="flex flex-col justify-between lg:flex-row">
+        <div className="flex overflow-x-auto">
+          <StrikeGraph />
+        </div>
+        <div className="mt-4 flex space-x-2 lg:mt-0 lg:block lg:space-x-0 lg:space-y-2">
           <Button variance="text" active>
             2025년
           </Button>
