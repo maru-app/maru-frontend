be2df5215a458c0d78ebb6765e6b17ff04449828
Daegyeom Ha <combbm@gmail.com>
2025-02-10 14:43:08 +0900

    feat: add style for mobile screen in Navbar and Container components

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,7 +2,7 @@ import { FC, HTMLAttributes, PropsWithChildren } from 'react';
 import { cn } from '@/utils/cn';
 
 const Container: FC<PropsWithChildren<HTMLAttributes<unknown>>> = ({ children, className }) => {
-  return <div className={cn('mx-auto max-w-screen-md xl:max-w-screen-xl', className)}>{children}</div>;
+  return <div className={cn('mx-auto max-w-screen-lg px-6 lg:px-0 xl:max-w-screen-xl', className)}>{children}</div>;
 };
 
 export default Container;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { FC, useMemo } from 'react';
+import { FC, useMemo, useState } from 'react';
 import Container from '@/components/Container';
 import Button from '@/components/Button';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { PAGE_ROUTES } from '@/constants/route';
+import { Bars3Icon } from '@heroicons/react/24/solid';
+import { cn } from '@/utils/cn';
 
 interface NavbarProps {
   readonly authorize?: boolean;
@@ -15,15 +17,17 @@ const Navbar: FC<NavbarProps> = ({ authorize }) => {
   const pathname = usePathname();
   const notAuthorizeRoute = useMemo(() => PAGE_ROUTES.filter((route) => !route.isAuthorized), []);
   const authorizeRoute = useMemo(() => PAGE_ROUTES.filter((route) => route.isAuthorized), []);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownStyle = isDropdownOpen ? 'block' : 'hidden';
 
   return (
-    <header className="fixed w-full border-b bg-white/50 py-3 backdrop-blur">
-      <Container className="flex items-center justify-between">
+    <header className="fixed w-full border-b bg-white/50 backdrop-blur">
+      <Container className="flex items-center justify-between py-3">
         <div className="flex">
-          <Link className="mr-16 select-none font-hahmlet text-[26px] font-bold" href="/">
+          <Link className="mr-16 select-none font-hahmlet text-2xl font-bold lg:text-[26px]" href="/">
             마루
           </Link>
-          <div className="flex space-x-6">
+          <div className="hidden space-x-6 lg:flex">
             <div className="flex space-x-4">
               {notAuthorizeRoute.map((route) => (
                 <Button key={route.href} variance="text" active={pathname === route.href} as={Link} href={route.href}>
@@ -51,14 +55,48 @@ const Navbar: FC<NavbarProps> = ({ authorize }) => {
             )}
           </div>
         </div>
+
         <Button
-          className="bg-emerald-500 text-sm font-bold text-white hover:bg-emerald-600"
+          className="hidden bg-emerald-500 text-sm font-bold text-white hover:bg-emerald-600 lg:block"
           as={Link}
           href={authorize ? '/write' : '/login'}
         >
           {authorize ? '일기쓰기' : '로그인'}
         </Button>
+        <Button variance="text" className="block lg:hidden" onClick={() => setIsDropdownOpen((prev) => !prev)}>
+          <Bars3Icon className="size-6" />
+        </Button>
       </Container>
+
+      <section className={cn('hidden w-full border-t p-4 lg:hidden', dropdownStyle)}>
+        <div className="space-y-2">
+          {notAuthorizeRoute.map((route) => (
+            <Button
+              key={route.href}
+              className="w-full text-left"
+              variance="text"
+              active={pathname === route.href}
+              as={Link}
+              href={route.href}
+            >
+              {route.name}
+            </Button>
+          ))}
+          {authorize &&
+            authorizeRoute.map((route) => (
+              <Button
+                key={route.href}
+                className="w-full text-left"
+                variance="text"
+                active={pathname === route.href}
+                as={Link}
+                href={route.href}
+              >
+                {route.name}
+              </Button>
+            ))}
+        </div>
+      </section>
     </header>
   );
 };
