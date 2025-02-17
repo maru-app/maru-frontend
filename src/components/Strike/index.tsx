244d3621aceccd331db0206a0507d54443e8bbb7
Daegyeom Ha <combbm@gmail.com>
2025-02-18 02:37:30 +0900

    feat: add mockup data

--- src/components/Strike/index.tsx
+++ src/components/Strike/index.tsx
@@ -1,8 +1,12 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useState } from 'react';
 import StrikeGraph from '@/components/StrikeGraph';
 import Button from '@/components/Button';
 
 const Strike: FC = () => {
+  const [year, setYear] = useState<number>(2025);
+
   return (
     <div className="mt-2 w-full rounded-md border border-gray-200 p-4">
       <h3 className="mb-3 text-xl font-bold">
@@ -10,13 +14,15 @@ const Strike: FC = () => {
       </h3>
       <div className="flex flex-col justify-between lg:flex-row">
         <div className="flex overflow-x-auto">
-          <StrikeGraph />
+          <StrikeGraph year={year} />
         </div>
         <div className="mt-4 flex space-x-2 lg:mt-0 lg:block lg:space-x-0 lg:space-y-2">
-          <Button variance="text" active>
+          <Button variance="text" active={year === 2025} onClick={() => setYear(2025)}>
             2025년
           </Button>
-          <Button variance="text">2024년</Button>
+          <Button variance="text" active={year === 2024} onClick={() => setYear(2024)}>
+            2024년
+          </Button>
         </div>
       </div>
     </div>
--- src/components/StrikeGraph/index.tsx
+++ src/components/StrikeGraph/index.tsx
@@ -1,12 +1,34 @@
 import { FC } from 'react';
+import { cn } from '@/utils/cn';
 
-const StrikeGraph: FC = () => {
+interface StrikeGraphProps {
+  readonly year?: number;
+}
+
+const DAY_MAP = ['일', '월', '화', '수', '목', '금', '토'];
+
+const StrikeGraph: FC<StrikeGraphProps> = ({ year }) => {
+  const getWeekOfYear = () => {
+    const today = new Date();
+    const dayNum = today.getUTCDay() || 7;
+    today.setUTCDate(today.getUTCDate() + 4 - dayNum);
+    const yearStart = new Date(Date.UTC(year ?? today.getUTCFullYear(), 0, 1));
+    return Math.ceil(((+today - +yearStart) / 86400000 + 1) / 7);
+  };
+
   return (
     <div className="space-y-0.5">
       {new Array(7).fill(0).map((_, index) => (
-        <div key={`row-${index}`} className="flex space-x-0.5">
-          {new Array(50).fill(0).map((_, j) => (
-            <div key={`row-${index}-${j}`} className="h-4 w-4 rounded-md bg-gray-300/70" />
+        <div key={`row-${index}`} className="flex justify-center space-x-0.5">
+          <span className="-mt-0.5 mr-3 text-sm text-gray-500">{DAY_MAP[index]}</span>
+          {new Array(getWeekOfYear()).fill(0).map((_, j) => (
+            <div
+              key={`row-${index}-${j}`}
+              className={cn(
+                'h-4 w-4 rounded-md bg-gray-300/70',
+                Math.random() > 0.5 ? 'bg-emerald-500/70' : 'bg-gray-300/70'
+              )}
+            />
           ))}
         </div>
       ))}
