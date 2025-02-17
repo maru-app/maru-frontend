0d057d0015dbf8a0d031097c2d3353d9ab0c5913
Daegyeom Ha <combbm@gmail.com>
2025-02-17 17:29:11 +0900

    feat: implement uploading image handler in Editor component

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -2,36 +2,93 @@
 
 import 'react-quill-new/dist/quill.snow.css';
 
-import { FC } from 'react';
+import { FC, RefObject, useCallback, useMemo, useRef } from 'react';
 import dynamic from 'next/dynamic';
+import ReactQuill from 'react-quill-new';
+import { generateGetPresignedUrl, generatePutPresignedUrl } from '@/api/mutation/file-mutation';
+import toast from 'react-hot-toast';
+import ReactQuillProps = ReactQuill.ReactQuillProps;
 
 interface EditorProps {
   readonly value: string;
   readonly onChange: (value: string) => void;
 }
 
-const QuillWrapper = dynamic(() => import('react-quill-new'), {
-  ssr: false,
-  loading: () => <div className="h-96 w-full border border-gray-200" />
-});
-
-const modules = {
-  toolbar: [
-    [{ header: '1' }, { header: '2' }, { header: '3' }],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-    [{ color: [] }, { background: [] }],
-    [{ align: [] }],
-    ['link', 'image'],
-    ['clean']
-  ],
-  clipboard: {
-    matchVisual: false
+const QuillWrapper = dynamic(
+  async () => {
+    const { default: Component } = await import('react-quill-new');
+    return ({ forwardedRef, ...props }: { forwardedRef: RefObject<ReactQuill | null> } & ReactQuillProps) => (
+      <Component ref={forwardedRef} {...props} />
+    );
+  },
+  {
+    ssr: false,
+    loading: () => <div className="h-96 w-full border border-gray-200" />
   }
-};
+);
 
 const Editor: FC<EditorProps> = ({ value, onChange }) => {
-  return <QuillWrapper theme="snow" modules={modules} value={value} onChange={onChange} />;
+  const editorRef = useRef<ReactQuill | null>(null);
+  const imageHandler = useCallback(() => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+    input.addEventListener('change', async () => {
+      try {
+        const file = input.files?.[0];
+        if (file === undefined) throw new Error('Not exists file.');
+        if (editorRef.current === null) throw new Error("Editor doesn't exist.");
+        const editor = editorRef.current.getEditor();
+        const range = editor.getSelection();
+        if (range === null) throw new Error("Range doesn't exist.");
+
+        const presignedUrl = await generatePutPresignedUrl(file.name);
+        if (presignedUrl.result === undefined) throw new Error('Presigned URL Error.');
+        const { url, fileName } = presignedUrl.result;
+
+        const uploadImageToBucket = await fetch(url, {
+          method: 'PUT',
+          body: file
+        });
+
+        if (uploadImageToBucket.ok) {
+          const getPresignedUrl = await generateGetPresignedUrl(fileName);
+          if (getPresignedUrl.result === undefined) throw new Error('Presigned URL Error.');
+
+          editor.insertEmbed(range.index, 'image', getPresignedUrl.result.url);
+        } else {
+          throw new Error('Image upload failed.');
+        }
+      } catch (error) {
+        toast.error('사진 업로드를 실패했어요. 잠시후 다시 시도해주세요.');
+      }
+    });
+  }, []);
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: '1' }, { header: '2' }, { header: '3' }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+          [{ color: [] }, { background: [] }],
+          [{ align: [] }],
+          ['link', 'image'],
+          ['clean']
+        ],
+        handlers: {
+          image: imageHandler
+        }
+      },
+      clipboard: {
+        matchVisual: false
+      }
+    }),
+    [imageHandler]
+  );
+
+  return <QuillWrapper forwardedRef={editorRef} theme="snow" modules={modules} value={value} onChange={onChange} />;
 };
 
 export default Editor;
