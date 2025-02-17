99197f1f8e2741899e9c8e63858a961ba00159af
Daegyeom Ha <combbm@gmail.com>
2025-02-17 19:31:46 +0900

    feat: implement diary-preprocessor

---
+++ src/utils/diary-preprocessor.ts
@@ -0,0 +1,23 @@
+'use server';
+
+import { generateGetPresignedUrl } from '@/api/mutation/file-mutation';
+
+export async function editorPreprocessor(rawContent: string) {
+  const bucketHost = process.env.NEXT_PUBLIC_STATIC_BUCKET_HOST ?? '';
+  const escapedBucketHost = bucketHost.replace(/[.*+?^${}()|[\]\\\/]/g, '\\$&');
+  const regex = new RegExp(`<img[^>]*src=["']${escapedBucketHost}([^"?]+)[^"']*["'][^>]*>`, 'g');
+  return rawContent.replace(regex, '[image|$1]');
+}
+
+export async function viewerPreprocessor(rawContent: string) {
+  const imageRegex = /\[image\|([^\]]+)\]/g;
+  let match;
+  while ((match = imageRegex.exec(rawContent)) !== null) {
+    const [original, fileName] = match;
+    const { result: presignedUrl } = await generateGetPresignedUrl(fileName);
+    if (presignedUrl !== undefined) {
+      rawContent = rawContent.replace(original, `<img src="${presignedUrl.url}" alt="image" />`);
+    }
+  }
+  return rawContent;
+}
