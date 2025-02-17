cff473ba247546e96943f733ca659d0d4e4fb30c
Daegyeom Ha <combbm@gmail.com>
2025-02-17 17:29:41 +0900

    feat: implement file mutation

---
+++ src/api/mutation/file-mutation.ts
@@ -0,0 +1,37 @@
+'use server';
+
+import { API_HOST } from '@/api';
+import { getCommonFetchConfig } from '@/api/config';
+import { ApiResponse } from '@/api/type/common.type';
+
+interface GeneratePutPresignedUrlReturn {
+  readonly url: string;
+  readonly fileName: string;
+}
+
+export const generatePutPresignedUrl = async (
+  originalFileName: string
+): Promise<ApiResponse<GeneratePutPresignedUrlReturn>> => {
+  const data = await fetch(`${API_HOST}/file/upload`, {
+    method: 'POST',
+    body: JSON.stringify({ originalFileName }),
+    ...(await getCommonFetchConfig())
+  });
+  return data.json();
+};
+
+interface GenerateGetPresignedUrlReturn {
+  readonly url: string;
+  readonly fileName: string;
+}
+
+export const generateGetPresignedUrl = async (
+  fileName: string
+): Promise<ApiResponse<GenerateGetPresignedUrlReturn>> => {
+  const data = await fetch(`${API_HOST}/file/download`, {
+    method: 'POST',
+    body: JSON.stringify({ fileName }),
+    ...(await getCommonFetchConfig())
+  });
+  return data.json();
+};
