3f94aeada524ce35ea560552fa3e8b8b6247915b
Daegyeom Ha <combbm@gmail.com>
2025-02-18 03:00:53 +0900

    fix: update layout of Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,20 +1,26 @@
 import { FC } from 'react';
 import Container from '@/components/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: FC = () => {
   return (
     <footer className="mt-48 h-72 border-t">
-      <Container className="flex h-full flex-col justify-center">
-        <h1 className="font-hahmlet text-xl font-bold">마루</h1>
-        <p className="ml-[1px] text-lg">나만의 일기를 쓰는 공간.</p>
-        <a
-          className="ml-[1px] mt-4 text-sm"
-          href="https://github.com/SkyLightQP/maru-frontend"
-          target="_blank"
-          rel="noreferrer"
-        >
-          View source on GitHub
-        </a>
+      <Container className="flex h-full items-center justify-between">
+        <div>
+          <h1 className="font-hahmlet text-xl font-bold">마루</h1>
+          <p className="ml-[1px] text-gray-400">나만의 일기를 쓰는 공간.</p>
+        </div>
+        <div>
+          <a
+            href="https://github.com/SkyLightQP/maru-frontend"
+            target="_blank"
+            rel="noreferrer"
+            className="transition ease-in-out hover:text-gray-500"
+          >
+            <FontAwesomeIcon icon={faGithub} className="size-5" />
+          </a>
+        </div>
       </Container>
     </footer>
   );
