c291090c5012463d90b2b526ddc28ebed77dc243
Daegyeom Ha <combbm@gmail.com>
2025-02-23 21:01:54 +0900

    fix: change icon instead of text in PageBackwardButton component

--- src/components/PageBackwardButton/index.tsx
+++ src/components/PageBackwardButton/index.tsx
@@ -3,11 +3,16 @@
 import { FC } from 'react';
 import Button from '@/components/Button';
 import { useRouter } from 'next/navigation';
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
 
 const PageBackwardButton: FC = () => {
   const router = useRouter();
 
-  return <Button onClick={() => router.back()}>뒤로가기</Button>;
+  return (
+    <Button onClick={() => router.back()} data-tooltip-id="tooltip" data-tooltip-content="뒤로가기">
+      <ChevronLeftIcon className="size-5" />
+    </Button>
+  );
 };
 
 export default PageBackwardButton;
