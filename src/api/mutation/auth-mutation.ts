42efedbe46a77c0cee78aa6c71b59994ca0ce3f3
Daegyeom Ha <combbm@gmail.com>
2025-02-26 14:15:52 +0900

    feat: add token cookie after register successfully

--- src/api/mutation/auth-mutation.ts
+++ src/api/mutation/auth-mutation.ts
@@ -18,11 +18,23 @@ export interface RegisterMutationParams {
   readonly registerToken: string;
 }
 export const registerMutation = async ({ nickname, registerToken }: RegisterMutationParams): Promise<void> => {
-  await fetch(`${API_HOST}/auth/register`, {
+  const response = await fetch(`${API_HOST}/auth/register`, {
     method: 'POST',
     body: JSON.stringify({ nickname }),
     ...(await getCommonFetchConfig({
       'X-Maru-RegisterToken': registerToken
     }))
+  });
+
+  const cookie = response.headers.get('set-cookie');
+  const token = cookie?.split(';');
+  token?.forEach((t) => {
+    const [key, value] = t.split('=');
+    if (key === '_maruToken') {
+      const cookieStore = cookies();
+      cookieStore.then((store) => {
+        store.set('_maruToken', value);
+      });
+    }
   });
 };
