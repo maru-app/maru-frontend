dfc1d594ac88e4fc73ed6f5d0d289642ec9ca6d1
Daegyeom Ha <combbm@gmail.com>
2025-02-26 21:23:58 +0900

    feat: change profile page to client component and implement update user

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,21 +1,46 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useCallback, useEffect, useState } from 'react';
 import PageTitle from '@/components/Typography/PageTitle';
 import Container from '@/components/Container';
-import { getMyInfoQuery } from '@/api/query/auth-query';
+import { getMyInfoQuery, GetMyInfoQueryReturn } from '@/api/query/auth-query';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 import { VENDOR } from '@/constants/vendor';
 import LogoutButton from '@/components/LogoutButton';
 import ProfileImage from '@/components/ProfileImage';
+import { updateUserMutation } from '@/api/mutation/user-mutation';
+import toast from 'react-hot-toast';
+import { EMOJI_LIST } from '@/constants/emoji';
 
-const Page: FC = async () => {
-  const myInfo = await getMyInfoQuery();
+const Page: FC = () => {
+  const [myInfo, setMyInfo] = useState<GetMyInfoQueryReturn | null>(null);
+
+  const fetchMyInfo = useCallback(() => {
+    getMyInfoQuery().then((data) => {
+      if (data.result === undefined || data.error) {
+        toast.error('내 정보를 가져올 수가 없어요. 잠시후 다시 시도해주세요.');
+        return;
+      }
+      setMyInfo(data.result);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchMyInfo();
+  }, []);
 
   const onSubmit = async (formData: FormData) => {
-    'use server';
-
     const nickname = formData.get('nickname');
     const streak = formData.get('streak');
+
+    await updateUserMutation({
+      nickname: String(nickname) ?? myInfo?.nickname
+    });
+
+    toast('내 정보를 변경했어요.', { icon: EMOJI_LIST.PENCIL });
+
+    fetchMyInfo();
   };
 
   return (
@@ -25,29 +50,36 @@ const Page: FC = async () => {
         <div className="mt-10">
           <p className="mb-1 text-lg font-bold">프로필 사진</p>
           <p className="mb-2 text-sm text-gray-400">* 프로필 사진 변경은 추후 제공 예정이에요.</p>
-          <ProfileImage nickname={myInfo.result?.nickname ?? ''} />
+          <ProfileImage nickname={myInfo?.nickname ?? ''} />
         </div>
+
         <div className="mt-10">
           <p className="mb-1 text-lg font-bold">연동된 소셜로그인</p>
-          <Input className="cursor-no-drop outline-none" defaultValue={VENDOR[myInfo.result?.vendor ?? '']} readOnly />
+          <Input className="cursor-no-drop outline-none" defaultValue={VENDOR[myInfo?.vendor ?? '']} readOnly />
         </div>
+
         <div className="mt-10">
           <p className="mb-1 text-lg font-bold">닉네임</p>
-          <Input name="nickname" defaultValue={myInfo.result?.nickname ?? ''} />
+          <Input name="nickname" defaultValue={myInfo?.nickname ?? ''} />
         </div>
+
         <div className="mt-10">
           <p className="mb-1 text-lg font-bold">연속 기록 공개여부</p>
+          <p className="mb-2 text-sm text-gray-400">
+            * 연속 기록 랭킹은 추후 제공 예정이에요. 기본값은 비공개로 설정돼요.
+          </p>
           <div className="flex space-x-4">
             <span className="space-x-2">
-              <input type="radio" name="streak" id="public" value="public" defaultChecked />
+              <input type="radio" name="streak" id="public" value="public" disabled />
               <label htmlFor="public">공개</label>
             </span>
             <span className="space-x-2">
-              <input type="radio" name="streak" id="private" value="private" />
+              <input type="radio" name="streak" id="private" value="private" defaultChecked disabled />
               <label htmlFor="private">비공개</label>
             </span>
           </div>
         </div>
+
         <div className="mt-10 flex justify-end space-x-2">
           <LogoutButton />
           <Button type="button" className="bg-red-500 text-white hover:bg-red-600">
