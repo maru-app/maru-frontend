4cc08a757c16483425e7cb54b5f0838b57d21099
Daegyeom Ha <combbm@gmail.com>
2025-02-27 10:34:42 +0900

    feat: implement `deleteUserMutation()`

--- src/api/mutation/user-mutation.ts
+++ src/api/mutation/user-mutation.ts
@@ -2,6 +2,7 @@
 
 import { API_HOST } from '@/api';
 import { getCommonFetchConfig } from '@/api/config';
+import { cookies } from 'next/headers';
 
 export interface UpdateUserMutationParams {
   readonly nickname: string;
@@ -12,4 +13,13 @@ export const updateUserMutation = async ({ nickname }: UpdateUserMutationParams)
     body: JSON.stringify({ nickname }),
     ...(await getCommonFetchConfig())
   });
+};
+
+export const deleteUserMutation = async (): Promise<void> => {
+  await fetch(`${API_HOST}/user`, {
+    method: 'DELETE',
+    ...(await getCommonFetchConfig())
+  });
+  const cookieStore = await cookies();
+  cookieStore.delete('_maruToken');
 };
