b1e68eb74284328b63c16d46c72977d25b57e9e3
Daegyeom Ha <combbm@gmail.com>
2025-02-27 10:34:20 +0900

    feat: add `forwardRef` in Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,15 @@
-import { FC, InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 import { cn } from '@/utils/cn';
 
-const Input: FC<InputHTMLAttributes<unknown>> = ({ className, ...rest }) => {
+const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<unknown>>(({ className, ...rest }, ref) => {
   return (
     <input
+      ref={ref}
       type="text"
       className={cn('w-full rounded-md border border-gray-300 bg-gray-50 p-2 lg:w-1/2', className)}
       {...rest}
     />
   );
-};
+});
 
 export default Input;
