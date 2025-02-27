c78ade6a92e1fa79a9350182d1e808ed92fa64a8
Daegyeom Ha <combbm@gmail.com>
2025-02-27 15:35:15 +0900

    feat: render strike icon instead of mockup data

--- src/components/StrikeGraph/index.tsx
+++ src/components/StrikeGraph/index.tsx
@@ -1,37 +1,48 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { cn } from '@/utils/cn';
 
 interface StrikeGraphProps {
-  readonly year?: number;
+  readonly year: number;
 }
 
 const DAY_MAP = ['일', '월', '화', '수', '목', '금', '토'];
 
 const StrikeGraph: FC<StrikeGraphProps> = ({ year }) => {
-  const getWeekOfYear = () => {
-    const today = new Date();
-    const dayNum = today.getUTCDay() || 7;
-    today.setUTCDate(today.getUTCDate() + 4 - dayNum);
-    const yearStart = new Date(Date.UTC(year ?? today.getUTCFullYear(), 0, 1));
-    return Math.ceil(((+today - +yearStart) / 86400000 + 1) / 7);
-  };
+  const daysSinceYear = useMemo(() => {
+    let startOfYear = new Date(year, 0, 1);
+    let today = new Date();
+    if (new Date().getFullYear() !== year) {
+      today = new Date(year, 11, 31);
+    }
+    const diffTime = today.getTime() - startOfYear.getTime();
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)) + 1;
+    return diffDays;
+  }, [year]);
+  const newYearDayIndex = useMemo(() => new Date(year, 0, 1).getDay(), [year]);
 
   return (
-    <div className="space-y-0.5">
-      {new Array(7).fill(0).map((_, index) => (
-        <div key={`row-${index}`} className="flex justify-center space-x-0.5">
-          <span className="-mt-0.5 mr-3 text-sm text-gray-500">{DAY_MAP[index]}</span>
-          {new Array(getWeekOfYear()).fill(0).map((_, j) => (
-            <div
-              key={`row-${index}-${j}`}
-              className={cn(
-                'h-4 w-4 rounded-md bg-gray-300/70',
-                Math.random() > 0.5 ? 'bg-emerald-500/70' : 'bg-gray-300/70'
-              )}
-            />
-          ))}
-        </div>
-      ))}
+    <div className="flex space-x-2">
+      <div className="mt-0.5">
+        {DAY_MAP.map((day) => (
+          <p key={`strike-day-${day}`} className="mt-[-1.8px] text-sm text-gray-500">
+            {day}
+          </p>
+        ))}
+      </div>
+      <div className="flex max-h-32 flex-col flex-wrap">
+        {new Array(newYearDayIndex).fill(0).map((_, idx) => (
+          <span key={`strike-empty-${idx}`} className="ml-0.5 mt-0.5 h-4 w-4 bg-transparent" />
+        ))}
+        {new Array(daysSinceYear).fill(0).map((_, idx) => (
+          <span
+            key={`strike-date-${idx}`}
+            className={cn(
+              'ml-0.5 mt-0.5 h-4 w-4 rounded-md bg-gray-300',
+              Math.random() > 0.5 ? 'bg-emerald-500/70' : 'bg-gray-300/70'
+            )}
+          />
+        ))}
+      </div>
     </div>
   );
 };
