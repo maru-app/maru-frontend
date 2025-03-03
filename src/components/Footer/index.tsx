3bbba935cde0df290b40a0ad5279211cf946da86
Daegyeom Ha <combbm@gmail.com>
2025-03-03 15:11:44 +0900

    feat: add hyperlink in Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import Container from '@/components/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import Link from 'next/link';
 
 const Footer: FC = () => {
   return (
@@ -12,14 +13,24 @@ const Footer: FC = () => {
           <p className="ml-[1px] text-gray-400">나만의 일기를 쓰는 공간.</p>
         </div>
         <div>
-          <a
-            href="https://github.com/SkyLightQP/maru-frontend"
-            target="_blank"
-            rel="noreferrer"
-            className="transition ease-in-out hover:text-gray-500"
-          >
-            <FontAwesomeIcon icon={faGithub} className="size-5" />
-          </a>
+          <div className="flex flex-col space-y-0.5 text-right">
+            <Link href="/tos" className="text-gray-400 hover:text-gray-500">
+              이용약관
+            </Link>
+            <Link href="/privacy" className="text-gray-400 hover:text-gray-500">
+              개인정보 처리방침
+            </Link>
+          </div>
+          <div className="mt-4 flex justify-end">
+            <a
+              href="https://github.com/SkyLightQP/maru-frontend"
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-400 transition ease-in-out hover:text-gray-500"
+            >
+              <FontAwesomeIcon icon={faGithub} className="size-5" />
+            </a>
+          </div>
         </div>
       </Container>
     </footer>
