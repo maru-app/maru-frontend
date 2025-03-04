fe29ea749725b83c1972d05b967f8e0eed206fe7
Daegyeom Ha <combbm@gmail.com>
2025-03-04 14:18:05 +0900

    chore: add icon resources

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,6 +8,18 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/?utm_source=pwa',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#10B981'
+    theme_color: '#10B981',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
   };
 }
