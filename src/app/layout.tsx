fb5a6bdde259184884f160818a0a8d67fb64ff33
Daegyeom Ha <combbm@gmail.com>
2025-03-04 11:11:54 +0900

    fix: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import 'react-tooltip/dist/react-tooltip.css';
 
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { FC, PropsWithChildren } from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
@@ -13,7 +13,12 @@ export const metadata: Metadata = {
     template: '%s | 마루 - 나만의 일기를 쓰는 공간',
     default: '마루 - 나만의 일기를 쓰는 공간'
   },
-  description: '마루는 나만의 일기를 쓸 수 있는 서비스입니다.'
+  description: '마루는 나만의 일기를 쓸 수 있는 서비스입니다.',
+  metadataBase: new URL('https://maruu.space')
+};
+
+export const viewport: Viewport = {
+  themeColor: '#10B981'
 };
 
 const RootLayout: FC<PropsWithChildren> = async ({ children }) => {
