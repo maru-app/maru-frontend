7562de1cf8774fc6ef0eab647f30420f2eff5962
Daegyeom Ha <combbm@gmail.com>
2025-03-11 20:27:14 +0900

    feat: add common header to get client ip

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,18 @@
-import { cookies } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 
-// eslint-disable-next-line no-undef
+export const getClientIpHeaders = async () => {
+  const headersList = await headers();
+  const forwardedFor = headersList.get('x-forwarded-for');
+  const clientIp = forwardedFor ? forwardedFor.split(',')[0].trim() : headersList.get('x-real-ip');
+
+  const clientIpHeader: Record<string, string> = {};
+  if (clientIp) {
+    clientIpHeader['X-Forwarded-For'] = clientIp;
+  }
+
+  return clientIpHeader;
+};
+
 export const getCommonFetchConfig = async (headerOption?: Record<string, string>): Promise<RequestInit> => {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get('_maruToken')?.value;
@@ -10,6 +22,7 @@ export const getCommonFetchConfig = async (headerOption?: Record<string, string>
       credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
+        ...(await getClientIpHeaders()),
         ...headerOption
       }
     };
@@ -20,6 +33,7 @@ export const getCommonFetchConfig = async (headerOption?: Record<string, string>
     headers: {
       'Content-Type': 'application/json',
       Cookie: `_maruToken=${accessToken}`,
+      ...(await getClientIpHeaders()),
       ...headerOption
     }
   };
