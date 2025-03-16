894e8dd1fad77ad6455d5a30cf035e732e5bfa89
Daegyeom Ha <combbm@gmail.com>
2025-03-16 17:27:50 +0900

    refactor: create `ApiResponse<T>` type and implement `createApiFetchError()`

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,21 @@
 export const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
+
+interface ApiOk<Response> {
+  readonly result: Response;
+}
+
+interface ApiFail {
+  readonly error: {
+    readonly code: string;
+  };
+}
+
+export type ApiResponse<Response> = Partial<ApiOk<Response> & ApiFail>;
+
+export const createApiFetchError = (): ApiFail => {
+  return {
+    error: {
+      code: 'FETCH_ERROR'
+    }
+  };
+};
