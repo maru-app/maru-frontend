2bd64dfebdb0003b169fcdd896ea3711d832aa3e
Daegyeom Ha <combbm@gmail.com>
2025-03-20 17:32:38 +0900

    feat: create MotionElement to use framer in server component

---
+++ src/components/MotionElement/index.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import button from '@/components/Button';
+
+export const MotionButton = motion.create(button);
+
+export const MotionH1 = motion.h1;
