3dfd81492568283db2cc0d5cdb189c417e983e44
Daegyeom Ha <combbm@gmail.com>
2025-03-20 17:32:46 +0900

    fix: update landing text

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -1,44 +1,118 @@
-'use client';
-
 import { FC } from 'react';
 import Container from '@/components/Container';
-import Button from '@/components/Button';
-import { motion } from 'framer-motion';
 import Link from 'next/link';
+import { getAllStreakQuery } from '@/api/query/streak-query';
+import { getDateString } from '@/utils/date';
+import { MotionButton, MotionH1 } from '../MotionElement';
 
-const MotionButton = motion.create(Button);
+const Jumbotron: FC = async () => {
+  const allStreak = await getAllStreakQuery(new Date().getFullYear());
+  const error = allStreak.error;
+  const todayStreak = allStreak.result?.find((s) => s.date === getDateString(new Date())) ?? undefined;
 
-const Jumbotron: FC = () => {
   return (
-    <section className="h-[28rem] bg-emerald-400">
+    <section className="h-[28rem] bg-[#C9E9D2]">
       <Container className="flex h-full flex-col justify-center">
         <div>
-          <motion.h1
-            initial={{ y: -50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.35, ease: 'easeOut' }}
-            className="text-3xl font-bold lg:text-4xl"
-          >
-            나만의 일기를 쓰는 공간,
-          </motion.h1>
-          <motion.h1
-            initial={{ y: -50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.45, ease: 'easeOut', delay: 0.18 }}
-            className="text-3xl font-bold lg:text-4xl"
-          >
-            여기는 <span className="font-hahmlet text-gray-50">마루</span>입니다.
-          </motion.h1>
-          <MotionButton
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.4, ease: 'easeOut', delay: 0.25 }}
-            className="mt-6 inline-block bg-black text-white hover:bg-stone-900"
-            as={Link}
-            href="/diary"
-          >
-            시작하기
-          </MotionButton>
+          <h1 className="mb-2">나만의 일기를 쓰는 공간, 마루</h1>
+          {error !== undefined && (
+            <>
+              <MotionH1
+                initial={{ y: -50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.35, ease: 'easeOut' }}
+                className="mb-1.5 text-3xl font-bold lg:text-4xl"
+              >
+                일기 내용 암호화{' '}
+                <span role="img" aria-label="자물쇠와 펜 이모지">
+                  🔏
+                </span>
+              </MotionH1>
+              <MotionH1
+                initial={{ y: -50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.45, ease: 'easeOut', delay: 0.18 }}
+                className="text-3xl font-bold lg:text-4xl"
+              >
+                마루에서 나만의 일기를&nbsp;
+                <br className="lg:hidden" />
+                완성해보세요
+              </MotionH1>
+              <MotionButton
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.4, ease: 'easeOut', delay: 0.25 }}
+                className="mt-8 inline-block bg-emerald-500 text-white hover:bg-emerald-600"
+                as={Link}
+                href="/login"
+              >
+                시작하기
+              </MotionButton>
+            </>
+          )}
+
+          {error === undefined && todayStreak === undefined && (
+            <>
+              <MotionH1
+                initial={{ y: -50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.35, ease: 'easeOut' }}
+                className="mb-1.5 text-3xl font-bold lg:text-4xl"
+              >
+                오늘 하루는 어떠셨나요?
+              </MotionH1>
+              <MotionH1
+                initial={{ y: -50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.45, ease: 'easeOut', delay: 0.18 }}
+                className="text-3xl font-bold lg:text-4xl"
+              >
+                마루에서 일상을 <br className="lg:hidden" />
+                기록해보세요
+              </MotionH1>
+              <MotionButton
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.4, ease: 'easeOut', delay: 0.25 }}
+                className="mt-8 inline-block bg-emerald-500 text-white hover:bg-emerald-600"
+                as={Link}
+                href="/write"
+              >
+                일기쓰기
+              </MotionButton>
+            </>
+          )}
+
+          {todayStreak && (
+            <>
+              <MotionH1
+                initial={{ y: -50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.35, ease: 'easeOut' }}
+                className="mb-1.5 text-3xl font-bold lg:text-4xl"
+              >
+                내일도 일기를 쓰고
+              </MotionH1>
+              <MotionH1
+                initial={{ y: -50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.45, ease: 'easeOut', delay: 0.18 }}
+                className="text-3xl font-bold lg:text-4xl"
+              >
+                연속 기록을 유지해보세요
+              </MotionH1>
+              <MotionButton
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.4, ease: 'easeOut', delay: 0.25 }}
+                className="mt-8 inline-block bg-emerald-500 text-white hover:bg-emerald-600"
+                as={Link}
+                href="/diary"
+              >
+                일기보기
+              </MotionButton>
+            </>
+          )}
         </div>
       </Container>
     </section>
