831e98c1ad66c27ef276b7270af4f99e487ebdd8
Daegyeom Ha <combbm@gmail.com>
2025-03-28 17:18:51 +0900

    fix: fix showing emoji in Apple devices

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
@@ -6,7 +7,7 @@ export default {
     extend: {
       fontFamily: {
         hahmlet: ['Hahmlet', 'serif'],
-        pretendard: ['Pretendard Variable', 'Fluent Emoji Color', 'sans-serif']
+        pretendard: ['Pretendard Variable', 'Fluent Emoji Color', ...defaultTheme.fontFamily.sans]
       }
     }
   },
